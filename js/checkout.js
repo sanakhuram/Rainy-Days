7a8b8b593f43e122a864d69f7cd094af9cc0aa4e
Sana Khuram <126725476+sanakhuram@users.noreply.github.com>
2024-04-18 11:23:04 +0200

    js files

---
+++ js/checkout.js
@@ -0,0 +1,10 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+  function displayMessage() {
+    const messageContainer = document.querySelector(".message-container");
+    messageContainer.style.display = "block";
+  }
+
+  setTimeout(displayMessage, 1000);
+});
+
